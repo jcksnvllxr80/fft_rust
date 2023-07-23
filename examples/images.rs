a7cc54ae93eb9048ae8e208d649a59f1f7d01682
Aaron Watkins <ac.watkins80@gmail.com>
2023-07-23 17:05:26 -0400

    add more images and change image code

--- examples/images.rs
+++ examples/images.rs
@@ -1,17 +1,29 @@
-use image::GenericImageView;
+use image::{EncodableLayout, GenericImageView};
 use image::io::Reader as ImageReader;
 
 fn main() {
-    let img = ImageReader::open("img/lenna.png").unwrap().decode().unwrap();
-    let sub_img = img.view(128,128,25,25).to_image();
-    println!("dimensions -> {:?}", sub_img.dimensions());
-    // println!("bytes -> {:?}", sub_img.as_bytes());
-    println!("small img -> {:?}", img.view(256,256,2,2).to_image());
-    println!("color -> {:?}", img.color());
-    println!("pixel (x0, y0) -> {:?}", img.get_pixel(256,256));
-    println!("pixel (x1, y0) -> {:?}", img.get_pixel(257,256));
-    println!("pixel (x0, y1) -> {:?}", img.get_pixel(256,257));
-    println!("pixel (x1, y1) -> {:?}", img.get_pixel(257,257));
+    // let img = ImageReader::open("img/lenna.png").unwrap().decode().unwrap();
+    // let img = ImageReader::open("img/airplane.bmp").unwrap().decode().unwrap();
+    let img = ImageReader::open("img/baboon.ppm").unwrap().decode().unwrap();
 
-    sub_img.save("img/output.jpg").unwrap();
+    let img_bytes = img.as_bytes();
+    let img_dim = img.dimensions();
+
+    let img_r_bytes: Vec<_> = img_bytes[..].iter().step_by(3).collect();
+    let img_g_bytes: Vec<_> = img_bytes[1..].iter().step_by(3).collect();
+    let img_b_bytes: Vec<_> = img_bytes[2..].iter().step_by(3).collect();
+
+    component_to_grey(String::from("grey_red_img"), img_r_bytes, img_dim);
+    component_to_grey(String::from("grey_green_img"), img_g_bytes, img_dim);
+    component_to_grey(String::from("grey_blue_img"), img_b_bytes, img_dim);
+}
+
+fn component_to_grey(out_file_name: String, img_bytes: Vec<&u8>, img_dim: (u32, u32)) {
+    let mut component_img = image::GrayImage::new(img_dim.0, img_dim.1);
+    for x in 0..img_dim.0 {
+        for y in 0..img_dim.1 {
+            component_img.put_pixel(x, y, image::Luma([*img_bytes[(y*img_dim.0 + x) as usize]]));
+        }
+    }
+    component_img.save(format!("img/{out_file_name}.jpg")).unwrap();
 }
